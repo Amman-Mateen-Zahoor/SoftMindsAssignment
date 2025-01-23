803607851b32c62250f61faa5dda76fcdb7e6287
Amman-Mateen-Zahoor <amman.W4u@gmail.com>
2025-01-23 18:52:41 +0500

    Done Example of Push notification by using fcm service

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
 // import Store from './Components/Redux/Store;
 import Axios_Practice from './Components/Axios_Practice';
+import messaging from '@react-native-firebase/messaging';
 
 // const AppRedux= () =>(
 //     <Provider store={Store} >
 //         <App/>
 //     </Provider>
 // )
-
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
 // AppRegistry.registerComponent(appName, () => AppRedux);
-AppRegistry.registerComponent(appName, () => Axios_Practice);
+AppRegistry.registerComponent(appName, () => App);
